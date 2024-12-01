ef879046609074ab9945a267a364e1352fa69889
Thomas <ming100628@gmail.com>
2024-12-01 10:14:59 +0800

    make File Links

--- server.js
+++ server.js
@@ -4,17 +4,43 @@ var os = require("os");
 const hostname = "127.0.0.1";
 const port = 3000;
 
+async function getAllFileNames() {
+  return fs.readdirSync("./public/");
+}
+
+function getFileData(filepath) {
+  return fs.readFileSync(filepath, "utf8");
+}
+
 const server = createServer(async (req, res) => {
   var s = "";
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
-  await fs.readdir("./public/", (err, files) => {
-    files.forEach((file) => {
-      res.write(file);
-      res.write("<br/>");
+  if (req.url === "/") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    await fs.readdir("./public/", (err, files) => {
+      files.forEach((file) => {
+        res.write(`<a href="/${file}/">${file}</a>`);
+        res.write("<br/>");
+      });
+      res.end();
     });
-    res.end();
-  });
+  } else {
+    var fileNames = await getAllFileNames();
+    var foundFile = false;
+    fileNames.forEach(async (file) => {
+      if (req.url.substring(1, req.url.length - 1) == file) {
+        const fileContent = getFileData(`./public/${file}`);
+        console.log(fileContent);
+        res.write(fileContent);
+        foundFile = true;
+        res.end();
+      }
+    });
+    if (!foundFile) {
+      res.statusCode = 404;
+      res.end();
+    }
+  }
   // res.end(s);
   // res.end(`${new Date()}`);
 });
